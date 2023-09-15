a9b42021ecd59cf4e1e0ee72138803b73fad232c
Ebrar Kadir Çetin <cetinebrarkadir@gmail.com>
2023-09-15 16:59:56 +0300

    enttiyRepository_Added

---
+++ src/OnlineExamSystem.EntityFrameworkCore/EntityFrameworkCore/Abstract/ITeacherDal.cs
@@ -0,0 +1,14 @@
+﻿using OnlineExamSystem.Data;
+using OnlineExamSystem.EntityFrameworkCore.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExamSystem.EntityFrameworkCore.Abstract
+{
+    public interface ITeacherDal : IEntityRepository<Teacher>
+    {
+    }
+}
---
+++ src/OnlineExamSystem.EntityFrameworkCore/EntityFrameworkCore/Repositories/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Abp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExamSystem.EntityFrameworkCore.Repositories
+{
+    public interface IEntityRepository <T> where T : class, IEntity
+    {
+        T GetByID(object id);
+        Task<T> GetByIDAsync(object id);
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null);
+        public void Add(T entity);
+        Task<T> AddAsync(T entity);
+        public void Update(T entity);
+        Task<T> UpdateAsync(T entity);
+        public void Delete(T entity);
+        Task<T> DeleteAsync(T entity);
+        public void DeleteByID(object id);
+    }
+}
